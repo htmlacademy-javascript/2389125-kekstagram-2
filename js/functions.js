14c02a2b33a93f0fd3c59590f71d39187210de41
Dmitriy Uzhakhov <alhimik.web@yandex.ru>
2025-05-27 18:21:38 +0300

    Добавляет исправления по функциям

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,33 @@
-// Функции для проверки длины строки.
-// Первая функция.
-
-function checkStringLength(string, maxLength) {
-  if (string.length <= maxLength) {
-    return true;
-  }
-  return false;
-}
-console.log(checkStringLength('Массив', 5));
-
-// Вторая функция (стрелочная).
+// Функция для проверки длины строки.
 
 const checkLength = (string = '', maxLength = 1) => string.length <= maxLength;
 console.log(checkLength('Функционал', 10));
 
-// Функции для проверки. Является ли строка палиндромом?
-// Первая функция
+// Функция для проверки. Является ли строка палиндромом?
 
-const isPalindrom = (string = '') => {
-  string = string.replaceAll(' ', '').toLowerCase();
+const isPalindrome = (string = '') => {
   let reverseString = '';
+  string = string.replaceAll(' ', '').toLowerCase();
+
   for (let i = string.length - 1; i >= 0; i--) {
     reverseString += string[i];
   }
+
   return string === reverseString;
 };
-console.log(isPalindrom('Лилипут сома на мосту пилил'));
+console.log(isPalindrome('Лилипут сома на мосту пилил'));
 
-// Вторая функция
+// Функция извлечения цифр из переданной строки
 
-const checkPalindrom = (string = '') => {
-  const normalString = string.replaceAll(' ', '').toUpperCase();
-  const reverseString = normalString.split('').reverse().join('');
-  return reverseString === normalString;
+const extractNumbers = (string = '') => {
+  let result = '';
+
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (Number.isNaN(parseInt(string[i], 10)) === false) {
+      result += string[i];
+    }
+  }
+
+  return result === '' ? NaN : Number(result);
 };
-console.log(checkPalindrom('Лилипут сома на мосту пилил'));
-
-
+console.log(extractNumbers('Мне 38'));
